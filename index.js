2f465016bd39360703cf33f03fe2235f80ab644e
Matti Luukkainen <mluukkai@iki.fi>
2023-03-22 14:03:33 +0200

    fix

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ let notes = [
 ]
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1><p>PERKELE</p>')
+  res.send('<h1>Hello World!</h1><p>PERKELE 2</p>')
 })
 
 app.get('/api/notes', (req, res) => {
